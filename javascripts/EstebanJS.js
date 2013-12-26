63314805cc8bf57ee2c88c9977d6b095145c1e51
esteban19 <EstebanMCG@gmail.com>
2013-12-25 19:28:56 -0500

    Version 1.0.0.0

---
+++ javascripts/EstebanJS.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    var imageName = ["/images/church.jpg", "/images/rain.jpg", "/images/nights.jpg", "/images/esteban_mcog.jpg"];
+    var indexNum = 0;
+
+    $("#picture").click(function () {
+        $("#picture").fadeOut(300, function () {
+            $("#picture").attr("src", imageName[indexNum]);
+            indexNum++;
+            if (indexNum > 3) { indexNum = 0; }
+            $("#picture").fadeIn(500);
+        });
+    });
+});
